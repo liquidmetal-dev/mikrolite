bbe7e4558f31f41732e7f3acc0efe55b2e248859
Richard Case <richard.case@outlook.com>
2023-11-08 07:57:42 +0000

    feat: define network service interface

---
+++ core/ports/network.go
@@ -0,0 +1,15 @@
+package ports
+
+type NetworkService interface {
+	BridgeCreate(name string) error
+	BridgeDelete(name string) error
+	BridgeExists(name string) (bool, error)
+
+	InterfaceCreate(name string, mac string) error
+	InterfaceDelete(name string) error
+	InterfaceExists(name string) (bool, error)
+
+	AttachToBridge(interfaceName string, bridgeName string) error
+
+	NewInterfaceName() (string, error)
+}
