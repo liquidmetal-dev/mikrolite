f4369e3763bbc37fd741d2bb7c02d9f937abb969
Richard Case <richard.case@outlook.com>
2023-11-08 08:55:18 +0000

    feat: added netlink based network service

---
+++ adapters/netlink/service.go
@@ -0,0 +1,55 @@
+package netlink
+
+import (
+	"errors"
+	"fmt"
+	"log/slog"
+
+	"github.com/mikrolite/mikrolite/core/ports"
+	"github.com/vishvananda/netlink"
+)
+
+const (
+	interfacePrefix = "mlt"
+)
+
+func New() ports.NetworkService {
+	return &networkService{}
+}
+
+type networkService struct {
+}
+
+func deleteLink(name string, linkType string) error {
+	link, err := netlink.LinkByName(name)
+	if err != nil {
+		if errors.Is(err, netlink.LinkNotFoundError{}) {
+			slog.Debug("network link not found, skipping deletion", "name", name, "type", linkType)
+
+			return nil
+		}
+
+		return fmt.Errorf("looking up %s %s: %w", linkType, name, err)
+	}
+
+	if err := netlink.LinkDel(link); err != nil {
+		return fmt.Errorf("deleting %s %s: %w", linkType, name, err)
+	}
+
+	return nil
+}
+
+func linkExists(name string, linkType string) (bool, error) {
+	if _, err := netlink.LinkByName(name); err != nil {
+		if errors.Is(err, netlink.LinkNotFoundError{}) {
+			slog.Debug("network link doesn't exist", "name", name, "type", linkType)
+
+			return false, nil
+		}
+
+		return false, fmt.Errorf("looking up %s: %w", name, err)
+	}
+
+	slog.Debug("network link exists", "name", name, "type", linkType)
+	return true, nil
+}
